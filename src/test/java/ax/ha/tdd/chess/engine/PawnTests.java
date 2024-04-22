f1c4b4dbcf1689d3e9c61e833ef580957ead17d5
Tobias Eriksson <tobias.eriksson@ha.ax>
2024-04-22 22:52:05 +0300

    Tests for pawn added

--- src/test/java/ax/ha/tdd/chess/engine/PawnTests.java
+++ src/test/java/ax/ha/tdd/chess/engine/PawnTests.java
@@ -6,8 +6,7 @@ import ax.ha.tdd.chess.engine.pieces.Pawn;
 import ax.ha.tdd.chess.engine.pieces.PieceType;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PawnTests {
 
@@ -24,5 +23,52 @@ public class PawnTests {
         //For debugging, you can print the board to console, or if you want
         //to implement a command line interface for the game
         System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+    @Test
+    public void testMovePawnOneStepForward() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("f3"));
+        chessboard.addPiece(pawn);
+
+        assertTrue(pawn.canMove(chessboard, new Square("f4")));
+    }
+
+    @Test
+    public void testMovePawnTwoStepsFromStartingPoint() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
+        chessboard.addPiece(pawn);
+
+        assertTrue(pawn.canMove(chessboard, new Square("e4")));
+    }
+
+    @Test
+    public void testMovePawnTwoStepsNotFromStartingPointShouldBeIllegal() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e4"));
+        chessboard.addPiece(pawn);
+
+        assertFalse(pawn.canMove(chessboard, new Square("e6")));
+    }
+
+    @Test
+    public void testMovePawnHorizontallyShouldBeIllegal() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e4"));
+        chessboard.addPiece(pawn);
+
+        assertFalse(pawn.canMove(chessboard, new Square("d4")));
+    }
+
+    @Test
+    public void testMoveToSpotOccupiedBySamePlayerShouldBeIllegal() {
+        Chessboard chessboard = new ChessboardImpl();
+        Pawn firstPawn = new Pawn(Color.WHITE, new Square("e4"));
+        Pawn secondPawn = new Pawn(Color.WHITE, new Square("e5"));
+        chessboard.addPiece(firstPawn);
+        chessboard.addPiece(secondPawn);
+
+        assertFalse(firstPawn.canMove(chessboard, new Square("e5")));
     }
 }
