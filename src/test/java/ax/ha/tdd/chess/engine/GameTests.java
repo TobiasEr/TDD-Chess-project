4e9ffb51c3727164e27e5c64b08f5e38e988fb00
Tobias Eriksson <tobias.eriksson@ha.ax>
2024-04-24 16:27:18 +0300

    Added move tests

--- src/test/java/ax/ha/tdd/chess/engine/GameTests.java
+++ src/test/java/ax/ha/tdd/chess/engine/GameTests.java
@@ -3,11 +3,24 @@ package ax.ha.tdd.chess.engine;
 import ax.ha.tdd.chess.console.ChessboardWriter;
 import ax.ha.tdd.chess.engine.pieces.ChessPiece;
 import ax.ha.tdd.chess.engine.pieces.PieceType;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class GameTests {
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(System.out);
+        System.setIn(System.in);
+    }
+
     @Test
     public void testMoveOpponentsPieceShouldBeIllegal() {
         //Arrange
@@ -27,5 +40,54 @@ public class GameTests {
         //For debugging, you can print the board to console, or if you want
         //to implement a command line interface for the game
         System.out.println(new ChessboardWriter().print(game.getBoard()));
+    }
+
+    @Test
+    public void testThatPieceIsActuallyMovedOnChessboardAfterMove() {
+        Game game = new GameImpl();
+
+        ChessPiece movingPiece = game.getBoard().getPieceAt(new Square("e2"));
+
+        game.move("e2-e4");
+
+        assertEquals(game.getBoard().getPieceAt(new Square("e4")), movingPiece);
+        assertNull(game.getBoard().getPieceAt(new Square("e2")));
+
+        //For debugging, you can print the board to console, or if you want
+        //to implement a command line interface for the game
+        System.out.println(new ChessboardWriter().print(game.getBoard()));
+    }
+
+    @Test
+    public void testThatPieceIsNotMovedOnChessboardAfterIllegalMove() {
+        Game game = new GameImpl();
+
+        ChessPiece sourcePiece = game.getBoard().getPieceAt(new Square("e2"));
+
+        game.move("e2-e5");
+
+        assertEquals(game.getBoard().getPieceAt(new Square("e2")), sourcePiece);
+        assertNull(game.getBoard().getPieceAt(new Square("e5")));
+
+        //For debugging, you can print the board to console, or if you want
+        //to implement a command line interface for the game
+        System.out.println(new ChessboardWriter().print(game.getBoard()));
+    }
+
+    @Test
+    public void testMessageAfterSuccessfulMove() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(outputStream));
+
+        String input = "" + System.lineSeparator();
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        Game game = new GameImpl();
+        game.move("e2-e4");
+
+        String expectedOutput = "Player tried to perform move: e2-e4";
+        assertEquals(expectedOutput, outputStream.toString().trim());
+        assertEquals(game.getLastMoveResult(), "Player moved piece from e2 to e4");
     }
 }
